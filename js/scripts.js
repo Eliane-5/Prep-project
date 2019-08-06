869765d84f2c571ea70ee58abbc584ee805e2187
Julia-Agasaro <agasarojulia@gmail.com>
2019-08-06 19:28:53 +0200

    added a footer and styled it

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-// Js
+// function resetInputs(){
+//     $("#name").val("");
+//     $("#email").val("");
+//     $("#message").val("");
+    
+// }
+
+$(document).ready(function(){
+$('.btn').click(function (event){
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message =$('#message').val();
+    if ((name !== '')&& (email !== '')&&(message !== '')){
+        alert(name + ' ' + 'We have received your message and we thank you for your time')
+    }else{
+        alert('please input your contact information')
+    }
+
+});
+
+});
