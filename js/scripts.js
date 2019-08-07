d1c51079d426a846a0f4ac686ecfa19342bf7c16
irahozadenyse <irahodeny1@gmail.com>
2019-08-07 14:36:24 +0200

    i added about us

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// Js
+// user interface logic
+
+$(document).ready(function(){
+    $(".text").hide();
+    $(".container").css({
+        opacity:1
+    });
+    $(".container").hover(function(){
+    $(this).css({opacity:.8});
+    $(".text").fadeIn();
+    },function(){
+        $(this).stop().css({opacity:1})
+        $(".text").fadeOut();
+    }
+    )
+})
+
