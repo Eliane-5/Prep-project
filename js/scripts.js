e51348d639cda67462c36797ee03d2a73b5df40e
irahozadenyse <irahodeny1@gmail.com>
2019-08-07 19:38:41 +0200

    i added indetation of thefiles

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // user interface logic
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".text").hide();
     $("#care").css({
-        opacity:1
+        opacity: 1
     });
-    $("#care").hover(function(){
-    $(this).css({opacity:.8});
-    $(".text").fadeIn();
-    },function(){
-        $(this).stop().css({opacity:1})
+    $("#care").hover(function () {
+        $(this).css({ opacity: .8 });
+        $(".text").fadeIn();
+    }, function () {
+        $(this).stop().css({ opacity: 1 })
         $(".text").fadeOut();
     }
     )
