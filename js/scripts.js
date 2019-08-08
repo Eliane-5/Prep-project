6c086d41d2ed77bc4ba17f9531bde87643222d82
irahozadenyse <irahodeny1@gmail.com>
2019-08-08 21:18:55 +0200

    adding the about us page

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,1 @@
 // user interface logic
-
-$(document).ready(function () {
-    $(".text").hide();
-    $("#care").css({
-        opacity: 1
-    });
-    $("#care").hover(function () {
-        $(this).css({ opacity: .8 });
-        $(".text").fadeIn();
-    }, function () {
-        $(this).stop().css({ opacity: 1 })
-        $(".text").fadeOut();
-    }
-    )
-})
-
